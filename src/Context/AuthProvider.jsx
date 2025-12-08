3905ffe7b71bec7875acbe6a32c8e003cd33fb5f
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-12-08 17:09:23 +0600

    make custom hooks and complete AuthProvider

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,10 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {auth} from '../firebase/firebase.init'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
+  const [loading, setLoading] = useState(true)
+  // Create user
+  const registerUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email , password)
+  }
+  // sing-in user with email and password
+  const singInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+  // logOut user
+  const logOut = () => {
+    setLoading(true);
+    return signOut (auth)
+  }
+  // update profile 
+  const updateUserProfile = (profile) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, profile)
+  }
+  //observe user state
+  useEffect(() => {
+    const unSubscibe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+      setLoading(false);
+      return () => {
+        unSubscibe();
+      };
+    });
+  }, []);
   const authInfo = {
+    registerUser,
+    singInUser,
+    logOut,
+    updateUserProfile,
     user,
+    loading
   };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
