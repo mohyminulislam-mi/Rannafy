369bb136afe46ef9d33847b00852ecac505822a2
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-12-09 01:21:10 +0600

    Make Testimonial section

--- src/Pages/Home/home/Home.jsx
+++ src/Pages/Home/home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Hero from "../Hero";
-import Meals from "./Meals/Meals";
+import Meals from "../Meals/Meals";
 import FeaturesSection from "../FeaturesSection";
+import Testimonial from "../Testimonial.jsx";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Meals />
       <FeaturesSection />
+      <Testimonial />
     </div>
   );
 };
