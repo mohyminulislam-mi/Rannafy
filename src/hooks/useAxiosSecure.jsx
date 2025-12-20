f0e347d624690aab3749d1812f8e9b4dc4eb7173
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-12-20 17:55:15 +0600

    added vercel server

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://rannafy-server.vercel.app",
 });
 const useAxiosSecure = () => {
   const { user, logOut } = useAuth();
